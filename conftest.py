0a17e114c6c1e37c676ba3aa1029c2f25d066223
BeardedVage <vagezakaryan@mail.ru>
2018-02-02 05:16:10 +0300

    conftest with config

---
+++ conftest.py
@@ -0,0 +1,11 @@
+import pytest
+from selenium import webdriver
+
+@pytest.fixture(scope="module", autouse=True)
+def driver(request):
+    browser = webdriver.Chrome()
+    browser.get("http://www.sberbank.ru/ru/quotes/converter")
+    browser.maximize_window()
+    browser.implicitly_wait(10)
+    yield browser
+    browser.quit()
